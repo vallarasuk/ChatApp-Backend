e98019f7feab20d415993cbecaebb85b5655faf4
vallarasuk <vallarasuk143@gmail.com>
2024-07-25 22:44:29 +0530

    feature/knexfile file uncomment commit

---
+++ knexfile.js
@@ -0,0 +1,29 @@
+module.exports = {
+    development: {
+      client: 'mysql',
+      connection: {
+        host: 'localhost',
+        user: 'root',
+        password: 'new_password',
+        database: 'chatapp'
+      },
+      migrations: {
+        directory: __dirname + '/db/migrations'
+      },
+      seeds: {
+        directory: __dirname + '/db/seeds'
+      }
+    },
+  
+    production: {
+      client: 'mysql',
+      connection: process.env.DATABASE_URL,
+      migrations: {
+        directory: __dirname + '/db/migrations'
+      },
+      seeds: {
+        directory: __dirname + '/db/seeds'
+      }
+    }
+  };
+  
